d08d7eb90dc096ec93aa909d96a1bfdb1f6b9e57
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-17 17:08:49 +0530

    Added Model in Server
    Added Model in Server

---
+++ ChatServer/ChatServer/Model/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.Model
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Msg{ get; set; }
+        public virtual User FromUserId { get; set; }
+        public virtual User ToUserId { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
