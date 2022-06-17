1062ad36b24c4a233253e81c11e1ae363add8448
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-17 17:12:19 +0530

    Updated the ChatHub with Unique Client
    Updated the ChatHub with Unique Client

--- ChatServer/ChatServer/Hubs/ChatHub.cs
+++ ChatServer/ChatServer/Hubs/ChatHub.cs
@@ -1,16 +1,61 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatServer.Business.Context;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatServer.Model;
+using System.Collections.Concurrent;
 
 namespace ChatServer.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string username, string message)
+        private readonly ILogger<ChatHub> _logger;
+        private readonly ChatContext _context;
+        private readonly static Dictionary<string, string> _ConnectionsMap = new Dictionary<string, string>();
+        public ChatHub(ILogger<ChatHub> logger, ChatContext dbContext)
         {
-            await Clients.All.SendAsync("ReceiveMessage", username, message);
+            _logger = logger;
+            _context = dbContext;
+        }
+
+        public async Task<bool> Login(string userid)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == userid);
+            if (user != null)
+            {
+                if (!_ConnectionsMap.ContainsKey(userid))
+                {
+                    _ConnectionsMap.Add(userid, Context.ConnectionId);
+                    await Clients.All.SendAsync("LoginMessage", userid);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public async Task<bool> LogOut(string userid)
+        {
+            if (_ConnectionsMap.ContainsKey(userid))
+            {
+                _ConnectionsMap.Remove(userid);
+                await Clients.All.SendAsync("LogoutMessage", userid);
+            }
+
+            return true;
+        }
+
+        public async void SendMessage(string userid, string message)
+        {
+            if (_ConnectionsMap.ContainsKey(userid))
+            {
+                var conectionid = _ConnectionsMap[userid];
+                await Clients.Client(conectionid).SendAsync("ReceiveMessage", userid, message);
+                await Clients.Caller.SendAsync("ReceiveMessage", userid, message);
+            }
         }
     }
 }
