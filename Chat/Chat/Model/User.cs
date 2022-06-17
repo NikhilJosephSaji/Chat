df722cb61dcd0f7ef1c9d442e8fa5638a55c96f2
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-17 17:10:53 +0530

    Added User Model in Chat
    Added User Model in Chat

---
+++ Chat/Chat/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Model
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailId { get; set; }
+    }
+}
