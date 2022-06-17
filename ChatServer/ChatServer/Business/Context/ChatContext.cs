55dea4521604442f2b7fd22e82e3f81f4f940cec
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-17 17:09:32 +0530

    Added Migration With Db
    Added Migration With Db

---
+++ ChatServer/ChatServer/Business/Context/ChatContext.cs
@@ -0,0 +1,19 @@
+﻿using ChatServer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.Business.Context
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Msgs { get; set; }
+    }
+}
---
+++ ChatServer/ChatServer/Migrations/20220616091357_InitialChatServerMigration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatServer.Migrations
+{
+    public partial class InitialChatServerMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Msgs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Msg = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FromUserIdId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ToUserIdId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Msgs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Msgs_Users_FromUserIdId",
+                        column: x => x.FromUserIdId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Msgs_Users_ToUserIdId",
+                        column: x => x.ToUserIdId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Msgs_FromUserIdId",
+                table: "Msgs",
+                column: "FromUserIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Msgs_ToUserIdId",
+                table: "Msgs",
+                column: "ToUserIdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Msgs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
