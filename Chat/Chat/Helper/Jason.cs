5d20c60ebbb8d457127ce922d74a269f5c8c8cbd
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-21 21:06:33 +0530

    Create Jason
    Create Jason

---
+++ Chat/Chat/Helper/Jason.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Helper
+{
+    public class Jason<T>
+    {
+        public T Deserialize(string obj)
+        {
+           return JsonConvert.DeserializeObject<T>(obj);
+        }
+
+        public string Serialize(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
