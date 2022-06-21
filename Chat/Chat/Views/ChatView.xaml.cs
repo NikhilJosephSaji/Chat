69f03c10ef93cd860d5cc53315fab61b56cdb98f
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-21 21:12:12 +0530

    Create ChatView.xaml.cs

---
+++ Chat/Chat/Views/ChatView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Chat.Model;
+using Chat.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chat.Views
+{
+    /// <summary>
+    /// Interaction logic for ChatView.xaml
+    /// </summary>
+    public partial class ChatView : Window
+    {
+        private ChatViewModel vm;
+        public ChatView(User user)
+        {
+            InitializeComponent();
+            vm = new ChatViewModel(user);
+            this.DataContext = vm;
+            vm.Close += ClosingWindow;
+        }
+
+        private void ClosingWindow()
+        {
+            this.Close();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            vm.LiistBoxHeight = (int)this.ActualHeight > 90 ? (int)this.ActualHeight - 90 : (int)this.ActualHeight;
+        }
+
+        private async void Window_Closed(object sender, EventArgs e)
+        {
+            await vm.LogoutUser();
+        }
+    }
+}
