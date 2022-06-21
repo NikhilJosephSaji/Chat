842d67b7ecc45a3dada7541760c60a20fdf87529
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-21 21:04:51 +0530

    Create Constants
    Create Constants

---
+++ Chat/Chat/Constants/ChatConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Constants
+{
+    public class ChatConstants
+    {
+        public const string Login = "/User/Login?username={0}&password={1}";
+        public const string GetUser = "/User/GetUser?userid={0}";
+        public const string PostUser = "User/Register";
+        public const string HTTPURL = "https://localhost:44386";
+        public const string URLCHAT = "https://localhost:44386/chathub";
+    }
+}
