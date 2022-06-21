3bdcb0562ab3adbc629265154d4e9956279abe6f
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-21 21:10:30 +0530

    Create Online user Model
    Create Online user Model

---
+++ Chat/Chat/Model/OnlineUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.ViewModel
+{
+    public class OnlineUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public bool HasSentNewMessage { get; set; }
+        public bool IsTyping { get; set; }
+    }
+}
