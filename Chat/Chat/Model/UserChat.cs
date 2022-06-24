465e711759102656556a0a4a60ca2486a96a1d4c
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-06-24 13:21:40 +0530

    Added UserChat.cs
    Added UserChat.cs

---
+++ Chat/Chat/Model/UserChat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Model
+{
+    public class UserChat
+    {
+        public string ChatUserImage { get; set; }
+        public string Picture { get; set; }
+        public string Author { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public bool IsNativeSender { get; set; }
+    }
+}
